878a3e4ce28f0726aafedaaba5f2cd7a9ab60c14
Thiágo Morais <35998529+moraisthiago@users.noreply.github.com>
2019-04-11 19:55:53 -0300

    Add files via upload

---
+++ ExercicioRevisão8.py
@@ -0,0 +1,3 @@
+base = int(input("Digite um valor inteiro para a base.\n")); expoente = int(input("Digite um valor inteiro para o expoente.\n"))
+
+print ("{} elevado a {} é {}." .format (base, expoente, base ** expoente))
