f7ded5656d66be301fb6c7f9486782c4f1417653
Thiágo Morais <35998529+moraisthiago@users.noreply.github.com>
2019-04-12 09:09:13 -0300

    Delete ExercicioRevisão2.py

--- ExercicioRevisão2.py
+++
@@ -1,9 +0,0 @@
-valores = list(map(int, input("Digite seis valores inteiros!\n").split()))
-valoresPositivos = int()
-
-for i in valores:
-	
-	if i > 0:		
-		valoresPositivos += 1
-		
-print ("Existem {} valores positivos.\n" .format( valoresPositivos))	
