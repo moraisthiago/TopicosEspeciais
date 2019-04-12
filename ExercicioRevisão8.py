d01b423bc8cdc0a984686215c783cef88e66b039
Thiágo Morais <35998529+moraisthiago@users.noreply.github.com>
2019-04-12 09:10:03 -0300

    Delete ExercicioRevisão8.py

--- ExercicioRevisão8.py
+++
@@ -1,3 +0,0 @@
-base = int(input("Digite um valor inteiro para a base.\n")); expoente = int(input("Digite um valor inteiro para o expoente.\n"))
-
-print ("{} elevado a {} é {}." .format (base, expoente, base ** expoente))
