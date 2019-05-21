ff5aa60d065081a604943223b5258e62a67e435c
IFPB <thiagueson@gmail.com>
2019-05-21 10:47:06 -0300

    Função

---
+++ MeuProjetoSimplesPython/app.py
@@ -0,0 +1,9 @@
+def somar(valor1, valor2):
+    return valor1 + valor2
+
+valor1 = int(input("Por favor, digite o valor do primeiro número.\n"))
+valor2 = int(input("Por favor, digite o valor do segundo número.\n"))
+
+resultado = somar (valor1, valor2)
+
+print ("O resultado da soma é {}." .format(resultado))
