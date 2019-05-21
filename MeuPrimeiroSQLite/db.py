53b6eb80fb6ace9f697fea4f779c3e81a6408896
IFPB <thiagueson@gmail.com>
2019-05-21 10:52:25 -0300

    Gerenciando SQLite3 com Python3

--- MeuPrimeiroSQLite/db.py
+++ MeuPrimeiroSQLite/db.py
@@ -5,15 +5,11 @@ conn = sqlite3.connect('shallownowschool.db')
 cursor = conn.cursor()
 
 cursor.execute("""
-    CREATE TABLE TB_ESTUDANTE(
-        ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        NOME VARCHAR(30) NOT NULL,
-        ENDERECO TEXT NOT NULL,
-        NASCIMENTO DATE NOT NULL,
-        MATRICULA VARCHAR(12) NOT NULL
-    );
+    SELECT * FROM TB_ESTUDANTE;
 """)
 
-print("Tabela criada com sucesso.")
+for linha in cursor.fetchall():
+    print(linha)
+
 
 conn.close()
